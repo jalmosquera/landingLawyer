5edffdb6203e7bfa9bc6b95243ca063cc7f455cc
Jalberth Mosquera <jmosquera2305@gmail.com>
2026-05-05 18:22:52 +0200

    fix(urls): use empty string to match root path for redoc view

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     # API Documentation
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     
     # API Endpoints
     path('api/auth/', include('apps.users.api.router')),
